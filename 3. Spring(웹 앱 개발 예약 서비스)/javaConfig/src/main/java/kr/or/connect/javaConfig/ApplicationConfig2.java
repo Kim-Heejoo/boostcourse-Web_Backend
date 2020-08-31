1c8c9b939ba880816c3e971a7c5da090d1c2f001
khJoo5 <delight_joo@naver.com>
2020-08-31 22:06:04 +0900

    Add javaConfig project
    [부스트코스] 웹 백엔드과정
    '3. Spring(웹 앱 개발 예약 서비스)' 내용 추가
    1.Spring Core
      4) Java Config를 이용한 설정

---
+++ 3. Spring(웹 앱 개발 예약 서비스)/javaConfig/src/main/java/kr/or/connect/javaConfig/ApplicationConfig2.java
@@ -0,0 +1,7 @@
+package kr.or.connect.javaConfig;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan("kr.or.connect.javaConfig")
+public class ApplicationConfig2 {
+}
---
+++ 3. Spring(웹 앱 개발 예약 서비스)/javaConfig/src/main/java/kr/or/connect/javaConfig/ApplicationContextExam03.java
@@ -0,0 +1,16 @@
+package kr.or.connect.javaConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextExam03 {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		   
+//		Car car = (Car)ac.getBean("car");
+		Car car = (Car)ac.getBean(Car.class);
+		car.run();
+		
+		
+	}
+}
