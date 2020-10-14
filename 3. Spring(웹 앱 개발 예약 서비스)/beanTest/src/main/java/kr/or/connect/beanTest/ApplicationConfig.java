81cf26ac7bce9a242a551a54d31f7d820e7bf083
khJoo5 <delight_joo@naver.com>
2020-10-14 21:11:55 +0900

    Add beanTest project
    [부스트코스] 웹 백엔드과정
    '3. Spring(웹 앱 개발 예약 서비스)' 내용 추가
    1.Spring Test
      3) 스프링 테스트 애노테이션 사용하기

---
+++ 3. Spring(웹 앱 개발 예약 서비스)/beanTest/src/main/java/kr/or/connect/beanTest/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package kr.or.connect.beanTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"kr.or.connect.beanTest"})
+public class ApplicationConfig {
+}
