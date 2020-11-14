92fb9031259f81db314bc230077648f5bffe63c8
khJoo5 <delight_joo@naver.com>
2020-11-14 23:53:56 +0900

    Add unitTest project
    [부스트코스] 웹 백엔드과정
    '3. Spring(웹 앱 개발 예약 서비스)' 내용 추가
    2. Spring Test
      4) 로직 단위테스트 구현하기

---
+++ 3. Spring(웹 앱 개발 예약 서비스)/unitTest/src/main/java/kr/or/connect/unitTest/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package kr.or.connect.unitTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"kr.or.connect.unitTest"})
+public class ApplicationConfig {
+}
